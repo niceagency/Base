5c6d275c3d98e2b76d28c72c822a47054a451911
Wain <wain@niceagency.co.uk>
2017-02-03 15:18:13 +0000

    Add a completion handler on data store init so we can know when the persistent store is ready.

--- Base/Datastore.swift
+++ Base/Datastore.swift
@@ -31,7 +31,7 @@ public final class Datastore {
     
     private var viewContext: NSManagedObjectContext
     
-    public init(withModelURL modelURL: URL, documentURL: URL, autoMigrate: Bool = false) {
+    public init(withModelURL modelURL: URL, documentURL: URL, autoMigrate: Bool = false, isReady: (() -> Void)? = nil) {
         guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
             fatalError("Error initializing mom from: \(modelURL)")
         }
@@ -62,6 +62,12 @@ public final class Datastore {
             }
             
             BaseLog.coreData.log(.trace, "Added store")
+            
+            if let completion = isReady {
+                DispatchQueue.main.async {
+                    completion()
+                }
+            }
         }
     }
     
