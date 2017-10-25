e534f3b6a89615e3c4792a24a9d2892b51f399a3
Wain <wain@niceagency.co.uk>
2017-10-25 17:02:34 +0100

    Addition of support for flow coordination with a superclass which can handle child coordinators and view controllers, a navigation controller and the concept of completion (when no children of any kind remain).

---
+++ Base/Coordinator.swift
@@ -0,0 +1,112 @@
+//
+//  Coordinator.swift
+//  Base
+//
+//  Created by Wain on 25/10/2017.
+//  Copyright © 2017 Nice Agency. All rights reserved.
+//
+
+import UIKit
+
+public class Coordinator {
+    public let navigationController: UINavigationController
+    
+    private weak var parent: Coordinator?
+    
+    private var childCoordinators: [Coordinator] = []
+    private var childViewControllers: [WeakViewController] = [] 
+    
+    public convenience init() {
+        self.init(navigationController: UINavigationController())
+    }
+    
+    public required init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    //MARK:
+    
+    @discardableResult public func setNavigationController(delegate: UINavigationControllerDelegate, overwrite: Bool) -> Bool {
+        guard navigationController.delegate == nil || overwrite else { return false }
+        
+        navigationController.delegate = delegate
+        return true
+    }
+    
+    public func pushChild(viewController vc: UIViewController, animated: Bool) {
+        navigationController.pushViewController(vc, animated: animated)
+        
+        childViewControllers.append(WeakViewController(vc: vc))
+        
+        destroyCompleteChildren()
+    }
+    
+    public func present(viewController vc: UIViewController, animated: Bool, completion: (() -> Void)?) {
+        navigationController.present(vc, animated: animated, completion: completion)
+        
+        childViewControllers.append(WeakViewController(vc: vc))
+        
+        destroyCompleteChildren()
+    }
+    
+    public func noteDidReturn(child vc: UIViewController) {
+        childViewControllers.append(WeakViewController(vc: vc))
+        
+        destroyCompleteChildren()
+    }
+    
+    //MARK:
+    
+    public func destroyCompleteChildren() {
+        childCoordinators.forEach({ $0.selfDestructIfPossible() })
+    }
+    
+    public func selfDestructIfPossible() {
+        destroyCompleteChildren()
+        
+        if !childViewControllers.contains(where: { $0.isValid }) {
+            guard childCoordinators.isEmpty else { return }
+            
+            notifyCompletion()
+        }
+    }
+    
+    //MARK:
+    
+    public func add(child: Coordinator) {
+        child.parent = self
+        childCoordinators.append(child)
+        
+        destroyCompleteChildren()
+    }
+    
+    private func remove(child: Coordinator) {
+        if let index = childCoordinators.index(where: { $0 === child }) {
+            childCoordinators.remove(at: index)
+        }
+        
+        selfDestructIfPossible()
+    }
+    
+    //MARK:
+    
+    public func notifyCompletion() {
+        parent?.didComplete(child: self)
+    }
+    
+    public func didComplete(child: Coordinator) {
+        remove(child: child)
+    }
+}
+
+private final class WeakViewController {
+    private weak var proxy: UIViewController?
+    
+    init(vc: UIViewController) {
+        proxy = vc
+    }
+    
+    var isValid: Bool {
+        return proxy != nil
+    }
+}
