facb1dc8e9e9be1347ff5cb25b8530e2b513059a
Tim Searle <tim.searle@nice.agency>
2018-05-01 11:52:05 +0100

    Updating dependencies to allow for App Extension API compliance.
    Marking BackgroundTaskBehavior as a sample reference - as it uses UIApplication.shared which is forbidden within app extensions.

--- Base/UIRequestBehavior.swift
+++ Base/UIRequestBehavior.swift
@@ -8,55 +8,34 @@
 
 import UIKit
 
-public final class BackgroundTaskBehavior: RequestBehavior {
-    
-    private let application = UIApplication.shared
-    
-    private var identifier: UIBackgroundTaskIdentifier?
-    
-    public func before(sending: URLRequest) {
-        identifier = application.beginBackgroundTask(expirationHandler: {
-            self.endBackgroundTask()
-        })
-    }
-    
-    public func after(completion: URLResponse?) {
-        endBackgroundTask()
-    }
-    
-    public func after(failure: Error?, retry: () -> Void) {
-        endBackgroundTask()
-    }
-    
-    private func endBackgroundTask() {
-        if let identifier = identifier {
-            application.endBackgroundTask(identifier)
-            self.identifier = nil
-        }
-    }
-}
+//
+// Sample BackgroundTaskBehavior
+//
 
-public final class NetworkActivityIndicatorBehavior: RequestBehavior {
-    
-    class ActivityIndicatorState {
-        var counter = 0 {
-            didSet {
-                UIApplication.shared.isNetworkActivityIndicatorVisible = self.counter == 0
-            }
-        }
-    }
-    
-    static let state = ActivityIndicatorState()
-    
-    public func before(sending: URLRequest) {
-        NetworkActivityIndicatorBehavior.state.counter += 1
-    }
-    
-    public func after(completion: URLResponse?) {
-        NetworkActivityIndicatorBehavior.state.counter -= 1
-    }
-    
-    public func after(failure: Error?, retry: () -> Void) {
-        NetworkActivityIndicatorBehavior.state.counter -= 1
-    }
-}
+//public final class BackgroundTaskBehavior: RequestBehavior {
+//    
+//    private let application = UIApplication.shared
+//    
+//    private var identifier: UIBackgroundTaskIdentifier?
+//    
+//    public func before(sending: URLRequest) {
+//        identifier = application.beginBackgroundTask(expirationHandler: {
+//            self.endBackgroundTask()
+//        })
+//    }
+//    
+//    public func after(completion: URLResponse?) {
+//        endBackgroundTask()
+//    }
+//    
+//    public func after(failure: Error?, retry: () -> Void) {
+//        endBackgroundTask()
+//    }
+//    
+//    private func endBackgroundTask() {
+//        if let identifier = identifier {
+//            application.endBackgroundTask(identifier)
+//            self.identifier = nil
+//        }
+//    }
+//}
