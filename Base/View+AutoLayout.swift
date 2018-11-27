ebbe985d71a3f544fd3d43e2106b754ae93c51fa
sam woolf <sam.woolf@gmail.com>
2018-11-27 11:05:12 +0000

    add centerIn, alightEdges and aspectRatio to auto layout support (#18)

--- Base/View+AutoLayout.swift
+++ Base/View+AutoLayout.swift
@@ -14,6 +14,8 @@ import Cocoa
 typealias View = NSView
 #endif
 
+// MARK: Constrainability
+
 public protocol Constrainable {
     var leadingAnchor: NSLayoutXAxisAnchor { get }
     var trailingAnchor: NSLayoutXAxisAnchor { get }
@@ -27,7 +29,7 @@ public protocol Constrainable {
     var centerYAnchor: NSLayoutYAxisAnchor { get }
     var firstBaselineAnchor: NSLayoutYAxisAnchor { get }
     var lastBaselineAnchor: NSLayoutYAxisAnchor { get }
-    
+
     #if os(iOS)
     var layoutMarginsGuide: UILayoutGuide { get }
     var readableContentGuide: UILayoutGuide { get }
@@ -35,12 +37,63 @@ public protocol Constrainable {
     @available(iOS 11.0, *)
     var safeAreaLayoutGuide: UILayoutGuide { get }
     #endif
+    
+    func centerIn(_ other: Constrainable, offsetBy offset: UIOffset) -> [NSLayoutConstraint]
+    func alignEdgesTo(_ other: Constrainable, insetBy insets: UIEdgeInsets) -> [NSLayoutConstraint]
+    func aspectRatio (_  ratio: CGFloat) -> [NSLayoutConstraint]
 }
 public typealias ConstrainedSuperview = Constrainable
 
+extension Constrainable {
+   
+    public func centerIn(_ other: Constrainable, offsetBy offset: UIOffset = UIOffset.zero) -> [NSLayoutConstraint] {
+        return [
+            self.centerYAnchor.constraint(equalTo: other.centerYAnchor, constant: offset.vertical),
+            self.centerXAnchor.constraint(equalTo: other.centerXAnchor, constant: offset.horizontal)
+        ]
+    }
+    
+    public func alignEdgesTo(_ other: Constrainable, insetBy insets: UIEdgeInsets = UIEdgeInsets.zero) -> [NSLayoutConstraint] {
+        
+        return [
+            self.leadingAnchor.constraint(equalTo: other.leadingAnchor, constant: insets.left),
+            self.trailingAnchor.constraint(equalTo: other.trailingAnchor, constant: insets.right),
+            self.topAnchor.constraint(equalTo: other.topAnchor, constant: insets.top),
+            self.bottomAnchor.constraint(equalTo: other.bottomAnchor, constant: insets.bottom)
+        ]
+    }
+    
+    public func aspectRatio(_  ratio: CGFloat) -> [NSLayoutConstraint] {
+    
+        return [
+            self.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: ratio)
+        ]
+    }
+}
+
 extension View: Constrainable {}
 
-public typealias ConstraintExpression = (_ make: Constrainable, _ superview: ConstrainedSuperview) -> [NSLayoutConstraint]
+// MARK: Collections of constraints
+
+public protocol ActivateableConstraint {
+    func activate()
+}
+
+extension NSLayoutConstraint: ActivateableConstraint {
+    public func activate() {
+        isActive = true
+    }
+}
+
+extension Array: ActivateableConstraint where Element: ActivateableConstraint {
+    public func activate() {
+        forEach { $0.activate() }
+    }
+}
+
+// MARK: Constraining
+
+public typealias ConstraintExpression = (_ make: Constrainable, _ superview: ConstrainedSuperview) -> [ActivateableConstraint]
 
 public extension View {
     public func constrain(by constrain: ConstraintExpression) {
@@ -52,6 +105,6 @@ public extension View {
         self.translatesAutoresizingMaskIntoConstraints = false
         let activations = constrain(self, superview)
         
-        activations.forEach { $0.isActive = true }
+        activations.forEach { $0.activate() }
     }
 }
